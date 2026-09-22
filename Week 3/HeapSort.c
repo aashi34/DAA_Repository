28b029accb4b9ade80414b6be56816ba11e56e76
Aashi Dixit <dixitaashi51@gmail.com>
2026-09-22 20:03:03 +0530

    q6

---
+++ Week 3/HeapSort.c
@@ -0,0 +1,76 @@
+/* Question: Heap Sort with Time Analysis
+Write a C program to implement Heap Sort and measure its execution time.
+*/
+#include <stdio.h>
+#include <time.h>
+
+void heapify(int arr[], int n, int i) {
+
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+
+    int n;
+    clock_t start, end;
+    double time_taken;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    start = clock();
+
+    heapSort(arr, n);
+
+    end = clock();
+
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Sorted Array: ");
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    printf("\nExecution Time = %f seconds\n", time_taken);
+
+    return 0;
+}
